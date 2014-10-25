88e4d7a597b3616f2c3fd4ef2ef8bfdb27a8ab4d
katreilly <kat.reilly@disney.com>
2014-10-25 11:19:32 -0700

    Initial Commit

---
+++ DNApp2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DNApp2
+//
+//  Created by Reilly, Kat on 10/25/14.
+//  Copyright (c) 2014 Kat Reilly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DNApp2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DNApp2
+//
+//  Created by Reilly, Kat on 10/25/14.
+//  Copyright (c) 2014 Kat Reilly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
